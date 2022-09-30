ade9b82c8c457ac128840e514c88a5c8ece5cab5
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-09-29 23:37:35 -0400

    Create and display todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,4 +5,22 @@ class TodosController < ApplicationController
     @todo = Todo.new
   end
 
+  def create
+    @todo= Todo.new(todo_params)
+
+    respond_to do |format|
+      if @todo.save
+        format.html { redirect_to todos_url, notice: "Todo was successfully created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description)
+  end
+
 end
